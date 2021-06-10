00c96b7a21af592bac8eb4d5ccc3196120bec33e
Neil Creveling <neil.creveling@gmail.com>
2021-06-10 11:53:07 -0400

    code handle functions in Filter.js

--- employee-directory/src/components/Filter.js
+++ employee-directory/src/components/Filter.js
@@ -1,2 +1,19 @@
 import React from "react";
 import "./style.css";
+
+const Filter = (props) => {
+
+    const handleFilter = () => {
+        props.filterEmployees( 'job' )
+        console.log('Filtered Employees')
+    }
+
+    const handleReset = () => {
+        props.resetFilter()
+        console.log('Filter Reset');
+    }
+
+    return (
+        
+    )
+}
