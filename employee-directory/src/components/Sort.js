f9636d2029c46b1c0a0cd9ed5625255b0ab32bb3
Neil Creveling <neil.creveling@gmail.com>
2021-06-10 11:47:24 -0400

    code handle functions in Sort.js

--- employee-directory/src/components/Sort.js
+++ employee-directory/src/components/Sort.js
@@ -1,2 +1,22 @@
 import React from "react";
 import "./style.css";
+
+const Sort = (props) => {
+
+    const handleFirstName = () => {
+        props.sortEmployeesByFirstName()
+        console.log("Sorted First Name");
+    }
+
+    const handleLastName = () => {
+        props.sortEmployeesByLastName()
+        console.log("Sorted Last Name")
+    }
+
+    return (
+        <div className='sort-div'>
+            
+        </div>
+    )
+
+};
