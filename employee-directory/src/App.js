7fd15f4d7e9ed650203bfc63f54f7f0022254e31
Neil Creveling <neil.creveling@gmail.com>
2021-06-11 17:49:15 -0400

    code initial filter and sort functions in app.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,7 +1,10 @@
-
 import './App.css';
 import React from 'react';
-
+import API from './components/utils/API';
+import Employee from './components/Employee';
+import Wrapper from './components/Wrapper';
+import Filter from './components/Filter';
+import Sort from './components/Sort';
 
 class App extends React.Component {
   state = {
@@ -9,7 +12,43 @@ class App extends React.Component {
     employees: [],
     search: ''
   };
+
+  componentDidMount() {
+    API.getEmployees()
+    .then(res => {
+      console.log( res.data.results );
+      this.setState({ employees: res.data.results });
+    })
+    .catch(err => console.log(err));
+  }
+
+  filterEmployees = () => {
+    const employees = this.state.employees.filter( employee => employee.dob.age < 50);
+    this.setState({ employees });
+  }
+
+  sortEmployeesByFirstNameAz = () => {
+    const employees = this.state.employees.sort((a,b)=>(a.name.first > b.name.first) ? 1 : ((b.name.first > a.name.first) ? -1 : 0));
+    this.setState({ employees });
+  }
+
+  sortEmployeesByFirstNameZa = () => {
+    const employees = this.state.employees.sort((a,b)=>(a.name.first < b.name.first) ? 1 : ((b.name.first < a.name.first) ? -1 : 0));
+    this.setState({ employees });
+  }
+
+  sortEmployeesByLastNameAz = () => {
+    const employees = this.state.employees.sort((a,b)=>(a.name.last > b.name.last) ? 1 : ((b.name.last > a.name.last) ? -1 : 0));
+    this.setState({ employees });
+  }
+
+  sortEmployeesByLastNameZa = () => {
+    const employees = this.state.employees.sort((a,b)=>(a.name.last < b.name.last) ? 1 : ((b.name.last < a.name.last) ? -1 : 0));
+    this.setState({ employees });
+  }
 }
+
+
 
 
 export default App;
