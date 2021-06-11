4765e94e61efe0f494856c7c7789ff7757d56adc
Neil Creveling <neil.creveling@gmail.com>
2021-06-11 18:09:27 -0400

    initial interface code for app.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -46,9 +46,54 @@ class App extends React.Component {
     const employees = this.state.employees.sort((a,b)=>(a.name.last < b.name.last) ? 1 : ((b.name.last < a.name.last) ? -1 : 0));
     this.setState({ employees });
   }
+
+  mapEmployeeRows = () => {
+    return this.state.employees.map( employee => (
+      <tr key={employee.phone}>
+        <th>{employee.name.first} {employee.name.last}</th>
+        <th>{employee.location.city}</th>
+        <th>{employee.dob.date}</th>
+        <th>{employee.dob.age}</th>
+      </tr>
+    ))
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <Title>Employee Directory</Title>
+          <nav className='d-flex align-items-baseline'>
+            <Filter
+              filterEmployees={this.filterEmployees}
+              resetFilter={this.resetFilter}
+            />
+          </nav>
+        </header>
+        <Table responsive striped bordered hover className="mt-1">
+          <thead>
+            <tr>
+              <th className="d-flex alight-items-center justify-content-between">
+                Name
+                <Sort
+                  sortEmployeesByFirstNameAz={ this.sortEmployeesByFirstNameAz }
+                  sortEmployeesByFirstNameZa={ this.sortEmployeesByFirstNameZa }
+                  sortEmployeesByLastNameAz={ this.sortEmployeesByLastNameAz }
+                  sortEmployeesByLastNameZa={ this.sortEmployeesByLastNameZa }
+                />
+              </th>
+              <th>Location</th>
+              <th>DOB</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.mapEmployeeRows() }
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
-
-
-
 
 export default App;
